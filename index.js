a6462610f806b12604f82fb33922ee2896a64a22
Jerome Miranda <jerome.m.miranda@gmail.com>
2019-05-10 13:34:20 +0800

    v0.0.2

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
 import React from 'react';
+import Checkbox from 'rc-checkbox';
+import Select from 'react-select';
+import { keyBy } from 'lodash';
 
 export default props => {
 	const { defaultValue = 'all', editable = false, id, onChange, ...defaultProps } = props;
 
+	const options = [
+		{ label: 'All', value: 'all' },
+		{ label: <i class="fa fa-check-circle" style={{ color: '#55b65c' }} />, value: true },
+		{ label: <i class="fa fa-times-circle" style={{ color: '#dc3545' }} />, value: false }
+	];
+	const optionsObj = keyBy(options, 'value');
+
 	return {
 		...defaultProps,
-		Cell: ({ value }) => {
-			const icon = value ? { symbol: 'check', color: '#55b65c' } : { symbol: 'times', color: '#dc3545' };
+		Cell: ({ original, value }) => {
+			if (editable) {
+				return (
+					<div style={{ textAlign: 'center' }}>
+						<Checkbox
+							checked={value}
+							onChange={e => onChange({ Id: original.Id, [id]: e.target.checked })}
+						/>
+					</div>
+				);
+			} else {
+				const icon = value ? { symbol: 'check', color: '#55b65c' } : { symbol: 'times', color: '#dc3545' };
+				return (
+					<div style={{ textAlign: 'center' }}>
+						<i class={`fa fa-${icon.symbol}-circle`} style={{ color: `${icon.color}` }} />
+					</div>
+				);
+			}
+		},
+		Filter: ({ filter, onChange }) => {
 			return (
-				<div style={{ textAlign: 'center' }}>
-					<i class={`fa fa-${icon.symbol}-circle`} style={{ color: `${icon.color}` }} />
-				</div>
+				<Select
+					isClearable
+					value={
+						filter
+							? optionsObj[filter.value]
+								? optionsObj[filter.value]
+								: optionsObj[defaultValue]
+							: optionsObj[defaultValue]
+					}
+					onChange={e => onChange(e ? (e.value != 'all' ? e.value : '') : '')}
+					options={options}
+				/>
 			);
 		}
-		// Filter: ({ filter, onChange }) => {
-		// 	return <span>jerome</span>;
-		// }
 	};
 };
-
-// function ColumnBoolean(columnProps) {
-// 	const { booleanProps, Menu, page, ...defaultProps } = columnProps;
-// 	return {
-// 		...defaultProps,
-// 		Cell: props => {
-// 			if (booleanProps && booleanProps.customAction) {
-// 				return <Menu value={props.value} menuProps={booleanProps} page={page} formId={props.original.Id} />;
-// 			} else {
-// 				let icon = props.value ? { symbol: 'check', color: '#55b65c' } : { symbol: 'times', color: '#dc3545' };
-// 				return (
-// 					<center>
-// 						<i
-// 							class={`fa fa-${icon.symbol}-circle boolean ${props.value}`}
-// 							style={{ color: `${icon.color}` }}
-// 						/>
-// 					</center>
-// 				);
-// 			}
-// 		}
-// 	};
-// }
