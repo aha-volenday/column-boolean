d89ba42989e8eeeef128c56e85ab555df8b02c16
Jerome Miranda <jerome.miranda@volenday.com>
2020-07-30 14:16:32 +0800

    v3.0.0

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
 import React from 'react';
 import { Checkbox, Radio } from 'antd';
 
-export default ({ editable = false, headerStyle = {}, id, onChange, style = {}, ...defaultProps }) => {
+export default ({ editable = false, id, onChange, ...defaultProps }) => {
 	return {
 		...defaultProps,
-		style: { ...style, display: 'flex', alignItems: 'center', justifyContent: 'center' },
-		headerStyle: {
-			...headerStyle,
-			overflow: editable ? 'visible' : 'hidden',
-			display: 'flex',
-			alignItems: 'center'
-		},
-		Cell: ({ original, value }) => {
+		Cell: ({ row, value }) => {
 			if (typeof value == 'undefined') return null;
 
-			if (editable) {
-				return (
-					<Checkbox checked={value} onChange={e => onChange({ Id: original.Id, [id]: e.target.checked })} />
-				);
-			}
+			if (editable)
+				return <Checkbox checked={value} onChange={e => onChange({ Id: row.Id, [id]: e.target.checked })} />;
 
 			const icon = value ? { symbol: 'check', color: '#55b65c' } : { symbol: 'times', color: '#dc3545' };
 			return <i class={`fa fa-${icon.symbol}-circle`} style={{ color: `${icon.color}` }} />;
 		},
-		Filter: ({ filter, onChange }) => {
+		Filter: ({ column: { filterValue, setFilter } }) => {
 			return (
 				<Radio.Group
-					onChange={e => onChange(e.target.value)}
-					value={typeof filter != 'undefined' ? filter.value : ''}>
+					onChange={e => setFilter(e.target.value)}
+					value={typeof filterValue !== 'undefined' ? filterValue : ''}>
 					<Radio value="">All</Radio>
 					<Radio value={true}>
 						<i class="fa fa-check-circle" style={{ color: '#55b65c' }} />
